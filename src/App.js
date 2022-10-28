098fb7ddbb81892b0c618cf02d60362c7c642c57
sneht <sneh.frontendarmy@gmail.com>
2022-10-28 10:27:01 +0530

    Change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import Navbar from "./Components/navbar/Index"
+import Navbar from "./Components/Navbar/Index"
 import { BrowserRouter } from "react-router-dom";
 import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
-import Home from './Components/home/index';
+import Home from './Components/Home/index';
 import Conatact from './Components/ContactMe/Index';
-import Sign from './Components/navbar/sign';
+import Sign from './Components/Navbar/sign';
 import Education from './Components/Education/Index';
 import Experience from './Components/Experience/Index';
 import Skills from './Components/Skills/Index';
