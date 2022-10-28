a6c5214596d25fdc20db883c4bcf85ce29d876ad
sneht <sneh.frontendarmy@gmail.com>
2022-10-28 12:59:10 +0530

    s

--- src/Components/ContactMe/Index.jsx
+++ src/Components/ContactMe/Index.jsx
@@ -14,13 +14,13 @@ const Conatact = () => {
           </div>
           <div>
             <a href={JsonFilee.contact.fb} className="me-4 link-secondary">
-              <i className="fab fa-facebook"></i>
+              <i className="bi bi-telephone"></i>
             </a>
             <a href={JsonFilee.contact.twitter} className="me-4 link-secondary">
               <i className="fab fa-twitter"></i>
             </a>
             <a href={JsonFilee.contact.gmail} className="me-4 link-secondary">
-              {/* <i className="bi-envelope-fill"></i> */}
+              <i className="bi-envelope-fill"></i>
             </a>
             <a href={JsonFilee.contact.ig} className="me-4 link-secondary">
               <i className="fab fa-instagram"></i>
