8cb1da2ce05cc321894968bc532deb8a368f4c12
sneht <sneh.frontendarmy@gmail.com>
2022-11-04 15:10:05 +0530

    a

--- src/Components/ContactMe/Index.jsx
+++ src/Components/ContactMe/Index.jsx
@@ -7,6 +7,7 @@ import { emailRegex } from "../../helper";
 import axios from "axios";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import "../../font.js";
 
 const Conatact = () => {
   AOS.init();
--- src/helper.js
+++ src/helper.js
@@ -5,5 +5,3 @@ export const emailRegex = /\S+@\S+\.\S+/;
 export const unameRegex = /\S+@\S+\.\S+/;
 export const passwordRegex = /^[A-z][A-z0-9-_]{8,23}$/;
 export const npointing=`https://63219772fd698dfa29fabd6e.mockapi.io/api/v1/`;
-
-export const pellete ={}
